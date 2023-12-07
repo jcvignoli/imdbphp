cc7ca252e0c959375c9a00195bb24edc8956cdf5
jcvignoli <dev@jcvignoli.com>
2023-12-07 17:32:00 -0500

    added new ImageProcessor class

--- bootstrap.php
+++ bootstrap.php
@@ -19,3 +19,4 @@ require_once __DIR__ . '/src/Imdb/PersonSearch.php';
 require_once __DIR__ . '/src/Imdb/Title.php';
 require_once __DIR__ . '/src/Imdb/TitleSearch.php';
 require_once __DIR__ . '/src/Imdb/TitleSearchAdvanced.php';
+require_once __DIR__ . '/src/Imdb/ImageProcessor.php';
